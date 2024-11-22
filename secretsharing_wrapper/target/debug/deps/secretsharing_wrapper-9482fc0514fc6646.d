3ed4072e83f7481594f44d51669d0e19c895ea7c
Gary Mawdsley <gm@datasignals.co.uk>
2024-11-22 15:56:08 +0000

    wrapper no longer needed - in any case it should not have contained any target directories!!!

--- secretsharing_wrapper/target/debug/deps/secretsharing_wrapper-9482fc0514fc6646.d
+++
@@ -1,5 +0,0 @@
-/Users/rameez/Desktop/secretsharing_wrapper/target/debug/deps/secretsharing_wrapper-9482fc0514fc6646: src/main.rs
-
-/Users/rameez/Desktop/secretsharing_wrapper/target/debug/deps/secretsharing_wrapper-9482fc0514fc6646.d: src/main.rs
-
-src/main.rs:
